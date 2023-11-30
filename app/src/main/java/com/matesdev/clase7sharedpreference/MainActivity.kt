f60ad5747f63311b4aadc0f6d0565b7e3552de04
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-11-30 00:14:02 -0300

    update mvp

--- app/src/main/java/com/matesdev/clase7sharedpreference/MainActivity.kt
+++ app/src/main/java/com/matesdev/clase7sharedpreference/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.matesdev.clase7sharedpreference
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,18 +8,30 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var buttonContinue: Button
+    private lateinit var btToRegister: Button
     private lateinit var textName: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonContinue = findViewById(R.id.btContinuar)
+        btToRegister = findViewById(R.id.btContinuar)
         textName = findViewById(R.id.tvNombre)
 
+        val preferences = getSharedPreferences("loginPref", MODE_PRIVATE)
+        val userName = preferences.getString("name", "")
 
-
-
+        if (userName != null) {
+            if(userName.isEmpty()){
+                btToRegister.text = "registrar"
+                btToRegister.setOnClickListener{
+                    val intent = Intent(this, RegisterActivity::class.java)
+                    startActivity(intent)
+                }
+            } else {
+                btToRegister.text = "continuar"
+                textName.text = userName
+            }
+        }
     }
 }
--- app/src/main/java/com/matesdev/clase7sharedpreference/RegisterActivity.kt
+++ app/src/main/java/com/matesdev/clase7sharedpreference/RegisterActivity.kt
@@ -1,11 +1,40 @@
 package com.matesdev.clase7sharedpreference
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var buttonSendToLogin : Button
+    private lateinit var etEnterName : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        buttonSendToLogin = findViewById(R.id.btEnviar)
+        etEnterName = findViewById(R.id.etUser)
+
+        buttonSendToLogin.setOnClickListener{
+            if(etEnterName.text.isNullOrEmpty()) {
+                Toast.makeText(this, "Ingrese un nombre válido", Toast.LENGTH_SHORT).show()
+            }else{
+
+                val userName = etEnterName.text.toString()
+                val preferences = getSharedPreferences("loginPref", MODE_PRIVATE)
+                val editor = preferences.edit().putString("name", userName)
+                editor.apply()
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+
+        }
+
     }
 }
