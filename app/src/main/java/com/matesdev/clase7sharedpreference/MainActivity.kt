256e9ac4518a6fda469cad8a3661ece6759326f4
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-11-29 23:29:19 -0300

    update backup

--- app/src/main/java/com/matesdev/clase7sharedpreference/MainActivity.kt
+++ app/src/main/java/com/matesdev/clase7sharedpreference/MainActivity.kt
@@ -2,10 +2,23 @@ package com.matesdev.clase7sharedpreference
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonContinue: Button
+    private lateinit var textName: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonContinue = findViewById(R.id.btContinuar)
+        textName = findViewById(R.id.tvNombre)
+
+
+
+
     }
 }
